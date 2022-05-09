e8b5846e9907d06d06d933312f2935c5db7ac2a9
tuanva <vuanhtuan743@gmail.com>
2022-05-09 17:18:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
